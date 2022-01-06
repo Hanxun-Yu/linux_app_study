f221dc3e6f5913684f1b57364eede2a7c82f7742
yuhanxun <yuhanxun@126.com>
2022-01-06 20:26:36 +0800

    sync

--- 4.fileio/copy.c
+++ 4.fileio/copy.c
@@ -18,8 +18,7 @@
 int main(int argc, char **argv)
 {
 	int fd_old, fd_new;
-	char buf[1024];
-	int len;
+	
 	
 	/* 1. 判断参数 */
 	if (argc != 3) 
@@ -39,6 +38,7 @@ int main(int argc, char **argv)
 	/* 3. 创建新文件
 	int open(const char *pathname, int flags, mode_t mode);
 
+	flag:
 	a. O_RDWR表示可读可写方式打开;
 	b. O_RDONLY表示只读方式打开;
 	c. O_WRONLY表示只写方式打开;
@@ -46,7 +46,14 @@ int main(int argc, char **argv)
 	e. O_TRUNC表示如果这个文件中本来是有内容的，则原来的内容会被丢弃，截断；
 	f. O_CREAT表示当前打开文件不存在，我们创建它并打开它，通常与O_EXCL结合使用，当没有文件时创建文件，有这个文件时会报错提醒我们；
 
+	mode:
 	Mode表示创建文件的权限，只有在flags中使用了O_CREAT时才有效，否则忽略。
+	S_IRUSR 文件所属用户 读
+	S_IWUSR 文件所属用户 写
+	S_IRGRP 与文件所属用户同组 读
+	S_IWGRP 与文件所属用户同组 写
+	S_IROTH 其他用户 读
+	S_IWOTH 其他用户 写
 
 	 */ 
 	fd_new = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
@@ -57,8 +64,13 @@ int main(int argc, char **argv)
 	}
 
 	/* 4. 循环： 读老文件-写新文件 */
+	char buf[1024]; //缓冲区
+	int len; //读取长度
+
+	//每次从老文件最多读取1024个字节到buf，实际读取为长度len
 	while ((len = read(fd_old, buf, 1024)) > 0)
 	{
+		//若len与写入长度不同，说明出错了
 		if (write(fd_new, buf, len) != len)
 		{
 			printf("can not write %s\n", argv[2]);
